4c02a8bb7b3d7794aaa865543a699d49843a76f5
Paweł Żurawka <pzurawka@gmail.com>
2019-10-22 15:22:39 +0200

    Add FAQ

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 // import Home from './components/Home/Home';
-// import Faq from './components/FAQ/Faq';
+import Faq from './components/FAQ/Faq';
 import Rules from './components/Rules/Rules';
 import Contact from './components/Contact/Contact';
 // import NotFound from './components/NotFound/NotFound';
@@ -21,8 +21,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path={'/rules'} component={Rules} />
           {/* <Route exact path={'/'} component={Home} /> */}
-          {/* <Route exact path={'/home'} component={Home} />
-            <Route exact path={'/faq'} component={Faq} /> */}
+          {/* <Route exact path={'/home'} component={Home} /> */}
+          <Route exact path={'/faq'} component={Faq} />
           <Route exact path={'/rules'} component={Rules} />
           <Route exact path={'/contact'} component={Contact} />
           {/* <Route exact path={'/cart'} component={Cart} />
---
+++ src/components/FAQ/Faq.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+// Import styles
+import './Faq.scss';
+
+const Faq = () => (
+  <div className='faq'>
+    <h1 className='faq__header'>Frequently Asked Questions</h1>
+    <ol className='faq__list'>
+      <li className='faq__question'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iste
+        cum quae quia assumenda libero soluta?
+        <p className='faq__answer'>
+          Proin mi augue, interdum pretium vehicula non, eleifend eu risus.
+          Maecenas quis volutpat ante. Vestibulum magna quam, volutpat ut nibh
+          sit amet, finibus suscipit metus. Suspendisse eu suscipit sem.
+        </p>
+      </li>
+      <li className='faq__question'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iste
+        cum quae quia assumenda libero soluta?
+        <p className='faq__answer'>
+          Proin mi augue, interdum pretium vehicula non, eleifend eu risus.
+          Maecenas quis volutpat ante. Vestibulum magna quam, volutpat ut nibh
+          sit amet, finibus suscipit metus. Suspendisse eu suscipit sem.
+        </p>
+      </li>
+      <li className='faq__question'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iste
+        cum quae quia assumenda libero soluta?
+        <p className='faq__answer'>
+          Proin mi augue, interdum pretium vehicula non, eleifend eu risus.
+          Maecenas quis volutpat ante. Vestibulum magna quam, volutpat ut nibh
+          sit amet, finibus suscipit metus. Suspendisse eu suscipit sem.
+        </p>
+      </li>
+      <li className='faq__question'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iste
+        cum quae quia assumenda libero soluta?
+        <p className='faq__answer'>
+          Proin mi augue, interdum pretium vehicula non, eleifend eu risus.
+          Maecenas quis volutpat ante. Vestibulum magna quam, volutpat ut nibh
+          sit amet, finibus suscipit metus. Suspendisse eu suscipit sem.
+        </p>
+      </li>
+      <li className='faq__question'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iste
+        cum quae quia assumenda libero soluta?
+        <p className='faq__answer'>
+          Proin mi augue, interdum pretium vehicula non, eleifend eu risus.
+          Maecenas quis volutpat ante. Vestibulum magna quam, volutpat ut nibh
+          sit amet, finibus suscipit metus. Suspendisse eu suscipit sem.
+        </p>
+      </li>
+    </ol>
+  </div>
+);
+
+export default Faq;
