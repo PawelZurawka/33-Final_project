81a17728d254b2ea32a6981de0a79893e8b2a4b7
Paweł Żurawka <pzurawka@gmail.com>
2019-11-11 20:03:32 +0100

    Edit store to fix devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,19 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import cartReducer from './cartRedux';
-import productReducer from './productRedux';
+import products from './productsRedux';
 
+//combine reducers
 const rootReducer = combineReducers({
-  products: productReducer,
+  products: products,
   cart: cartReducer
 });
 
+// create store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 export default store;
