14d1960e9549790b570ce55700555176cadc73ef
Paweł Żurawka <pzurawka@gmail.com>
2019-11-13 13:33:11 +0100

    Remove thunk from store

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,15 +1,17 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import cartReducer from './cartReducer'
-import productsReducer from './productReducer'
+import productReducer from './productReducer'
 
 //combine reducers
 const rootReducer = combineReducers({
-  products: productsReducer,
+  products: productReducer,
   cart: cartReducer,
 })
 
 // create store
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware()))
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
