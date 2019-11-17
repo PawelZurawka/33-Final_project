68860ea03e03de65be6f55fb34d0e9131fd045d7
Paweł Żurawka <pzurawka@gmail.com>
2019-11-17 18:44:54 +0100

    Fix problem with tag

--- src/components/ProductSummary/ProductSummary.js
+++ src/components/ProductSummary/ProductSummary.js
@@ -7,7 +7,7 @@ export const ProductSummary = props => (
   <Col className='product-summary' md={5}>
     <Link to={'/product/' + props.index} index={props.index}>
       <Card>
-        <Badge>{props.tag}</Badge>
+        {props.tag ? <Badge>{props.tag}</Badge> : <span></span>}
         <CardImg src={props.image} alt='product' />
         <CardTitle>{props.name}</CardTitle>
         <CardSubtitle>$ {props.price.toFixed(2)}</CardSubtitle>
