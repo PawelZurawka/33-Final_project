6efaff9636764b94a92b4538afe2d30e5f06d061
Paweł Żurawka <pzurawka@gmail.com>
2019-11-21 17:36:57 +0100

    Add footer to all pages to fix problem with router transition

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Footer from '../Footer/Footer'
 import './NotFound.scss'
 
 const NotFound = () => (
-  <div className='notfound'>
-    <img src={'/images/404-Error.png'} alt={'404 not found :('} />
-  </div>
+  <>
+    <div className='notfound'>
+      <img src={'/images/404-Error.png'} alt={'404 not found :('} />
+    </div>
+    <Footer />
+  </>
 )
 
 export default NotFound
