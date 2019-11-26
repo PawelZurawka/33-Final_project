73f82f18ed30823b7c6dcbe578596ba7335501ec
Paweł Żurawka <pzurawka@gmail.com>
2019-11-26 12:43:27 +0100

    Add cart counter

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import MainMenu from '../../components/layout/MainMenu/MainMenu'
 import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { connect } from 'react-redux'
 import './NavBar.scss'
 
 class NavBar extends React.Component {
@@ -29,6 +30,9 @@ class NavBar extends React.Component {
             activeClassName='active'
           >
             <FontAwesomeIcon icon={faShoppingCart} />
+            {this.props.cart.added && this.props.cart.added.length > 0 && (
+              <span>{this.props.cart.added.length}</span>
+            )}
           </NavLink>
         </div>
       </nav>
@@ -36,4 +40,10 @@ class NavBar extends React.Component {
   }
 }
 
-export default NavBar
+function mapStateToProps(state) {
+  return {
+    cart: state.cart,
+  }
+}
+
+export default connect(mapStateToProps)(NavBar)
